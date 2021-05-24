bcfd2914f111da67b5e59f56db3f063859b9f233
glubs9 <jonte.fry@gmail.com>
2021-05-24 20:41:17 +1000

    added join syntax example in examples.py

--- docs/examples.py
+++ docs/examples.py
@@ -181,3 +181,40 @@ def test():
 print("\nbefore test")
 test()
 print("after test\n")
+
+
+#basically function composition as syntax
+    #note: this function is very rubbish and doesn't return anything. probs could but this is cool enough as is
+def join(tuples):
+    ret = []
+    joining = False
+    for n in tuples:
+        inst, arg = n
+        if inst == "LOAD_CONST" and arg == '"join"':
+            joining = True
+        elif inst == "LOAD_CONST" and arg == '"end"':
+            joining = False
+        elif joining and (inst == "POP_TOP" or inst[:5] == "STORE"): #remove operations that pop from stack
+            continue
+        elif joining and inst == "CALL_FUNCTION": #add extra arg to call_function
+            ret.append((inst, str(int(arg)+1)))
+        elif joining and inst == "LOAD_NAME":
+            ret.append(n)
+            ret.append(("ROT_TWO", "0"))
+        else:
+            ret.append(n)
+    return ret
+
+def mult_two(n):
+    return n*2
+
+@macro(join)
+def test():
+    _ = "join"
+    n = 3
+    range(6)
+    sum()
+    mult_two()
+    print() #should print 24 because (3+4+5)*2 == 24
+    _ = "end"
+test()
--- test_file.py
+++ test_file.py
@@ -1,29 +1,26 @@
 from bytecodemacro import macro
 
 #basically function composition as syntax
+    #note: this function is very rubbish and doesn't return anything. probs could but this is cool enough as is
 def join(tuples):
     ret = []
     joining = False
     for n in tuples:
         inst, arg = n
-        if inst == "LOAD_CONST" and len(arg) > 5 and arg == "join":
+        if inst == "LOAD_CONST" and arg == '"join"':
             joining = True
-        elif inst == "LOAD_CONST" and arg == "end":
+        elif inst == "LOAD_CONST" and arg == '"end"':
             joining = False
         elif joining and (inst == "POP_TOP" or inst[:5] == "STORE"): #remove operations that pop from stack
             continue
         elif joining and inst == "CALL_FUNCTION": #add extra arg to call_function
-            ret.append((inst, int(arg)+1))
+            ret.append((inst, str(int(arg)+1)))
+        elif joining and inst == "LOAD_NAME":
+            ret.append(n)
+            ret.append(("ROT_TWO", "0"))
         else:
             ret.append(n)
-    for n in ret: print(n)
     return ret
-
-def range_list(start, end):
-    return list(range(start, end))
-
-def sum_list(li):
-    return sum(li)
 
 def mult_two(n):
     return n*2
@@ -32,9 +29,9 @@ def mult_two(n):
 def test():
     _ = "join"
     n = 3
-    range_list(6)
-    sum_list()
+    range(6)
+    sum()
     mult_two()
-    print()
+    print() #should print 24 because (3+4+5)*2 == 24
     _ = "end"
 test()
